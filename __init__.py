df1bcceba478c8bc5818ef4c61d8e2b7b1875c6f
ttwuddj-lang <ttwuddj@gmail.com>
2026-08-12 15:26:45 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,45 +1,25 @@
-import logging
+from on9wordchainbot.models.game import (
+    BannedLettersGame,
+    ChaosGame,
+    ChosenFirstLetterGame,
+    ClassicGame,
+    EliminationGame,
+    GAME_MODES,
+    HardModeGame,
+    MixedEliminationGame,
+    RequiredLetterGame,
+)
+from on9wordchainbot.models.player import Player
 
-from aiogram import Dispatcher
-from periodic import Periodic
-
-from on9wordchainbot.resources import init_resources, close_resources
-from on9wordchainbot.utils import send_admin_group
-from on9wordchainbot.words import Words
-
-try:
-    import coloredlogs
-except ImportError:
-    logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
-    logging.warning("coloredlogs not available; defaulting to logging. To install, use `pip install coloredlogs`.")
-else:
-    coloredlogs.install(fmt="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
-
-logger = logging.getLogger(__name__)
-
-try:
-    import uvloop
-except ImportError:
-    logger.warning("uvloop unavailable. To install, use `pip install uvloop`.")
-else:
-    uvloop.install()
-
-# ----- Initialize dispatcher -----
-from on9wordchainbot.handlers import routers
-from on9wordchainbot.handlers.errors import error_handler
-
-dp = Dispatcher()
-dp.include_routers(*routers)
-dp.error.register(error_handler)
-
-
-@dp.startup()
-async def startup():
-    await init_resources()
-    await Periodic(60 * 60, Words.update).start(delay=0)  # Run Words.update every hour
-    await send_admin_group("Bot starting.")
-
-@dp.shutdown()
-async def shutdown():
-    await close_resources()
-    await send_admin_group("Bot stopping.")
+__all__ = (
+    "Player",
+    "ClassicGame",
+    "HardModeGame",
+    "ChaosGame",
+    "ChosenFirstLetterGame",
+    "BannedLettersGame",
+    "RequiredLetterGame",
+    "EliminationGame",
+    "MixedEliminationGame",
+    "GAME_MODES"
+)
