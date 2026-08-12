09ad0843d6366f8ba236836013908d55c64c6d81
ttwuddj-lang <ttwuddj@gmail.com>
2026-08-12 15:27:15 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,22 +1,31 @@
-from on9wordchainbot.models.game import (
-    BannedLettersGame,
+from on9wordchainbot.models.game.banned_letters import BannedLettersGame
+from on9wordchainbot.models.game.chaos import ChaosGame
+from on9wordchainbot.models.game.chosen_first_letter import ChosenFirstLetterGame
+from on9wordchainbot.models.game.classic import ClassicGame
+from on9wordchainbot.models.game.elimination import EliminationGame
+from on9wordchainbot.models.game.hard_mode import HardModeGame
+from on9wordchainbot.models.game.mixed_elimination import MixedEliminationGame
+from on9wordchainbot.models.game.random_first_letter import RandomFirstLetterGame
+from on9wordchainbot.models.game.required_letter import RequiredLetterGame
+
+GAME_MODES = [
+    ClassicGame,
+    HardModeGame,
     ChaosGame,
     ChosenFirstLetterGame,
-    ClassicGame,
-    EliminationGame,
-    GAME_MODES,
-    HardModeGame,
-    MixedEliminationGame,
+    RandomFirstLetterGame,
+    BannedLettersGame,
     RequiredLetterGame,
-)
-from on9wordchainbot.models.player import Player
+    EliminationGame,
+    MixedEliminationGame
+]
 
 __all__ = (
-    "Player",
     "ClassicGame",
     "HardModeGame",
     "ChaosGame",
     "ChosenFirstLetterGame",
+    "RandomFirstLetterGame",
     "BannedLettersGame",
     "RequiredLetterGame",
     "EliminationGame",
---
+++ hard_mode.py
@@ -0,0 +1,13 @@
+from on9wordchainbot.models.game.classic import ClassicGame
+from on9wordchainbot.constants import GameSettings
+
+
+class HardModeGame(ClassicGame):
+    name = "hard mode game"
+    command = "starthard"
+
+    def __init__(self, group_id: int) -> None:
+        super().__init__(group_id)
+        # Hardest settings available
+        self.time_limit = GameSettings.MIN_TURN_SECONDS
+        self.min_letters_limit = GameSettings.MAX_WORD_LENGTH_LIMIT
