7089b768b660de691eb170ba9d978daf835e1d1d
ttwuddj-lang <ttwuddj@gmail.com>
2026-08-12 15:18:48 +0530

    Add files via upload

---
+++ __main__.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2025 TheHamkerAlone
+# Licensed under the MIT License.
+# This file is part of AloneXMusic
+
+
+import asyncio
+import importlib
+
+from pyrogram import idle
+
+from AloneX import (anon, app, appdb, config, db,
+                   logger, stop, userbot, yt)
+from AloneX.plugins import all_modules
+
+
+async def main():
+    await db.connect()
+    await appdb.connect()
+    await app.boot()
+    await userbot.boot()
+    await anon.boot()
+
+    for module in all_modules:
+        importlib.import_module(f"AloneX.plugins.{module}")
+    logger.info(f"Loaded {len(all_modules)} modules.")
+
+    if config.COOKIES_URL:
+        await yt.save_cookies(config.COOKIES_URL)
+
+    sudoers = await db.get_sudoers()
+    app.sudoers.update(sudoers)
+    app.bl_users.update(await db.get_blacklisted())
+    logger.info(f"Loaded {len(app.sudoers)} sudo users.")
+
+    await idle()
+    await stop()
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.get_event_loop().run_until_complete(main())
+    except KeyboardInterrupt:
+        pass
